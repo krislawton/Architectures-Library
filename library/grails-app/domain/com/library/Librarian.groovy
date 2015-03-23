37301807d649abf119214ab8084525862ced519a
krislawton <kris.lawton@gmail.com>
2015-03-23 17:05:20 +0000

    Library controller created

--- library/grails-app/domain/com/library/Librarian.groovy
+++ library/grails-app/domain/com/library/Librarian.groovy
@@ -1,7 +1,19 @@
 package com.library
 
 class Librarian {
+	String name
+	String email
+	String userName
+	String password
+	String telephone
+
+	String toString() {name}
 
     static constraints = {
+		name()
+		email()
+		userName()
+		password()
+		telephone()
     }
 }
