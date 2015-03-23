57aa964efad1df1cfae5f44b5e5aa900f0d3688f
krislawton <kris.lawton@gmail.com>
2015-03-23 19:11:29 +0000

    Added library-student relationship

--- library/grails-app/domain/com/library/Library.groovy
+++ library/grails-app/domain/com/library/Library.groovy
@@ -1,17 +1,21 @@
 package com.library
 
 class Library {
+	String name
 	String location
 	String openingHours
 	String book
-	String student
 	String librarian
 
-    static constraints = {
+	static hasMany=[students: Student]
+
+	String toString() {name}
+
+	static constraints = {
+		name()
 		location()
 		openingHours()
 		book()
-		student()
 		librarian()
     }
 }
